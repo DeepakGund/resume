25f251b0a152fdc8e0f930e136edc3297e61ee35
DeepakGund <126459378+DeepakGund@users.noreply.github.com>
2024-10-05 15:59:11 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,51 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "one" {
-count = 5
-ami = "ami-03eb6185d756497f8"
-instance_type = "t2.micro"
+resource "tls_private_key" "rsa_2048_example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "local_file" "private_key_pem" {
+filename = "key.pem"
+content = tls_private_key.rsa-2048_example.private_key_pem
+}
+
+
+locals {
+env = "swiggy"
+}
+
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.10.0.0/16"
+ tags = {
+    Name = "${local.env}-VPC"
+ }
+}
+
+
+
+resource "aws_subnet" "subnet" {
+vpc_id = aws_vpc.vpc.id
+cidr_block = "10.10.10.0/24"
+ tags = {
+    Name = "${local.env}-subnet"
+ }
+}
+
+resource "aws_instance" "ec2" {
+  count         = 5
+subnet_id = aws_subnet.subnet.id
+  ami           = "ami-01b4a58555824692b"
+  instance_type = "t2.micro"
+ tags = {
+    Name = "${local.env}-ec2"
+ }
+}
+
+
+output "abc" {
+value = aws_instance.ec2
 }
