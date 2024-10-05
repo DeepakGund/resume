f143241161cca8b69072789b0b00387b65f13730
DeepakGund <126459378+DeepakGund@users.noreply.github.com>
2024-10-05 15:40:43 +0530

    newmain.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,43 @@
-provider "aws"{
+provider "aws" {
 region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 2
-ami = "ami-03eb6185d756497f8"
-instance_type = "t2.micro"
+count = var.instance_count
+ami = "ami-0e001c9271cf7f3b9"
+instance_type = var.instance_type
+tags = {
+Name = var.instance_name
 }
+}
+
+cat variable.tf
+variable "instance_count" {
+}
+
+variable "instance_type" {
+}
+
+variable "instance_name" {
+}
+
+cat dev.tfvars
+instance_count = 1
+
+instance_type = "t2.micro"
+
+instance_name = "dev-server"
+
+cat test.tfvars
+instance_count = 2
+
+instance_type = "t2.medium"
+
+instance_name = "test-server"
+
+cat variable.tfvars
+instance_count = 3
+
+instance_type = "t2.large"
+
+instance_name = "prod-server"
