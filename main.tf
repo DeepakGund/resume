36ea5603fbcd2d099e95d20242232a40f3ba3a20
DeepakGund <126459378+DeepakGund@users.noreply.github.com>
2024-10-07 15:11:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,48 +3,46 @@ provider "aws" {
 }
 
 locals {
-env = "Teraform"
+  env = "Teraform"
 }
 
 #instance_count
-variable "instance_count"{
-default = 1
+variable "instance_count" {
+  default = 1
 }
 
 # instance_type
-variable "instance_type"{
-default = "t2.micro"
+variable "instance_type" {
+  default = "t2.micro"
 }
 
 # VPC
 resource "aws_vpc" "vpc" {
   cidr_block = "10.10.0.0/16"
- tags = {
+  tags = {
     Name = "${local.env}-VPC"
- }
+  }
 }
 
 # vpc-subnet
 resource "aws_subnet" "subnet" {
-vpc_id = aws_vpc.vpc.id
-cidr_block = "10.10.10.0/24"
- tags = {
-    Name = "${local.env}-subnet"
- }
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.10.10.0/24"
+  }
 }
 
 # EC2
 resource "aws_instance" "ec2" {
   count         = var.instance_count
-  subnet_id = aws_subnet.subnet.id
+  subnet_id     = aws_subnet.subnet.id
   ami           = "ami-01b4a58555824692b"
   instance_type = var.instance_type
- tags = {
+  tags = {
     Name = "${local.env}-Server-${count.index + 1}"
- }
+  }
 }
 
 
 output "abc" {
-value = aws_instance.ec2
+  value = aws_instance.ec2
 }
