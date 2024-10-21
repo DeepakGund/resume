49272c18dc1d35f0fd963a40fabed8d4e21e597d
DeepakGund <126459378+DeepakGund@users.noreply.github.com>
2024-10-21 19:48:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,28 @@
-vim main.tf 
-
 provider "aws" {
-region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "one" {
-ami = "ami-03eb6185d756497f8"
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "jenkins-VPC"
+  }
+}
+
+# vpc-subnet
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.10.10.0/24"
+  }
+
+# EC2
+resource "aws_instance" "ec2" {
+ami = "ami-01b4a58555824692b"
 instance_type = "t2.micro"
+}
+
+
+output "abc" {
+  value = aws_instance.ec2
 }
